1257623a5a7fd9eeccb7144549e9d27cfc7b538d
Meow6Woof <meow6woof@gmail.com>
2024-08-29 22:28:58 +0700

    create class dataBase

---
+++ app/dataBase/dataBase.php
@@ -0,0 +1,19 @@
+<?php 
+/**
+ * dataBase
+ * 
+ * @author Ivan Glibko 
+ * @version 1.0
+ */
+
+class dataBase{
+    private $dbHost;
+    private $dbName;
+    private $dbLogin;
+    private $dbPassword;
+
+    public function __construct(){
+        
+    }
+}
+?>

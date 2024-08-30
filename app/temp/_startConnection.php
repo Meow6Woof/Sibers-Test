7fd4b5fc7cefb23e811c9936d729659916a90a2c
Meow6Woof <meow6woof@gmail.com>
2024-08-30 14:02:00 +0700

    create temp/startConnection.php

---
+++ app/temp/_startConnection.php
@@ -0,0 +1,6 @@
+<?php 
+//Connection of libraries
+require '../vendor/autoload.php';
+use Firebase\JWT\JWT;
+    
+?>
--- public/index.php
+++ public/index.php
@@ -1,4 +1,3 @@
 <?php 
-require_once('../app/temp/_startConnection.php');
-    
+require_once '../app/temp/_startConnection.php';
 ?>
